e4127f6f78d5ee4d517b229e2a2044dfb0666df6
fenmay <mayer.mayer.real@yandex.ru>
2021-10-29 22:18:14 +0300

    4 page

--- src/js/main.js
+++ src/js/main.js
@@ -22,14 +22,33 @@ const swiper = new Swiper('#cat_med', {
 const swiper2 = new Swiper('#sorts', {
   // Optional parameters
   direction: 'horizontal',
-  // loop: true,
-  // infinite: false, 
+  
 
   slidesPerView: 5,
   centeredSlides: true,
   initialSlide: 2,
   spaceBetween: 40,
   slideActiveClass: 'swiper-slide-active',
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  
+});
+
+const swiper3 = new Swiper('#new_home', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 3,
+  spaceBetween: 40,
+  centeredSlides: true,
+  initialSlide: 1,
+  
 
   // Navigation arrows
   navigation: {
