a91816ae70553b0f567e7adb7b9e541234feda59
fenmay <mayer.mayer.real@yandex.ru>
2022-01-22 22:22:56 +0300

    begin

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,26 @@
-//here will by JavaScript code ...
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'progressbar',
+    },
+
+    slidesPerView: 4,
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    
+    watchSlidesProgress: true,
+    
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
